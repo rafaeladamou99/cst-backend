a4f9a9b8bf5ef1414fefef041fc6d80986ac884a
rafaeladamoukcl <k21217880@kcl.ac.uk>
2023-08-06 16:48:57 +0300

    Removed consolelog

--- src/calculations/Algorithms/maximumDegreev2.ts
+++ src/calculations/Algorithms/maximumDegreev2.ts
@@ -97,7 +97,6 @@ export const MaximumDegreeV2 = (
       .edges.sort((a, b) => a.weight - b.weight)
     sortPQ()
     PQ = [...newNodes, ...PQ]
-    console.log(PQ)
     PQ.some((e, index) => {
       if (
         degreePerNode[e.to] < maxDegree &&
